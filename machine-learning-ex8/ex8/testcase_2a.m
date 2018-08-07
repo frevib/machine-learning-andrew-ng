ab45ffb85f6027e6b8a6c9fa42181953525bce64
hielke de vries <hdevries@fastmail.com>
2018-08-07 21:19:12 +0200

    complete part2 week9

---
+++ machine-learning-ex8/ex8/testcase_2a.m
@@ -0,0 +1,7 @@
+% ---------------------------------------
+% Test 2a (Select threshold):
+% input:
+[epsilon F1] = selectThreshold([1 0 0 1 1]', [0.1 0.2 0.3 0.4 0.5]')
+% output:
+% epsilon =  0.40040
+% F1 =  0.57143
